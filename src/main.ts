20242983b1b3873e5b3e1413911d1978bd41f283
Git <104937558+Bodyuser@users.noreply.github.com>
2023-06-26 21:54:41 +0300

    Finish project 8

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,24 @@ import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { cors: true })
+	const app = await NestFactory.create(AppModule)
 	app.use(cookieParser())
 	app.setGlobalPrefix('api')
 	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*')
+		res.header('Access-Control-Allow-Origin', process.env.APP_DOMEN)
 		res.header(
 			'Access-Control-Allow-Methods',
 			'GET,PUT,POST,DELETE,OPTIONS,PATCH'
 		)
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+		res.header('Access-Control-Max-Age', 86400)
+		res.header('Access-Control-Allow-Headers', 'Content-Type')
+		res.header('Access-Control-Allow-Credentials', 'true')
 		next()
 	})
-	// app.enableCors({
-	// 	origin: true,
-	// 	credentials: true,
-	// })
+	app.enableCors({
+		origin: process.env.APP_DOMEN,
+		credentials: true,
+	})
 	await app.listen(4200)
 }
 bootstrap()
