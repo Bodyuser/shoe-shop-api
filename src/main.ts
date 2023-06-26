08136415583a809feadd8ad712f615b94af7a257
Git <104937558+Bodyuser@users.noreply.github.com>
2023-06-26 21:03:30 +0300

    Finish project 5

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 	app.use(cookieParser())
 	app.enableCors({
 		credentials: true,
-		origin: "*",
+		origin: true,
 	})
 	app.use((req, res, next) => {
 		res.header('Access-Control-Allow-Origin', '*')
